a1616efb9beea22af59d230874ce0d20cb03addd
avinavadiya <avinavadiya2000@gmail.com>
2023-05-10 13:16:08 +0530

    first commit

---
+++ enter_detail.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title>download chat</title>
+</head>
+
+<body>
+    <h3>Download Chat (Enter name of two person whose chat you want to download.)</h3>
+    <div class="form">
+        <form action="download.php" method="get">
+            User1 name: <input type="text" name="user1">
+            <div style="height: 30px;"></div>
+            User2 name: <input type="text" name="user2">
+            <div style="height: 30px;"></div>
+            <input type="submit" value="Download">
+        </form>
+    </div>
+</body>
+
+</html>
